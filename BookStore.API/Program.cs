a1a79fd6fa9e7220f9af2138ec6525435c1a005d
Aleshkevich Pavel <aleskevic79@gmail.com>
2022-12-22 12:02:36 +0300

    Add connection string

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,3 +1,5 @@
+using ApiBookStore.Extensions;
+
 namespace ApiBookStore
 {
     public class Program
@@ -12,6 +14,8 @@ namespace ApiBookStore
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.ConfigureMsSqlServerContext(builder.Configuration);
 
             var app = builder.Build();
 
