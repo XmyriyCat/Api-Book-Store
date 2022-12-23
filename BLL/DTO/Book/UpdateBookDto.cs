4672dcf9e4ea5314f4df7187dfa3b8542dd91b0c
Aleshkevich Pavel <aleskevic79@gmail.com>
2022-12-23 17:23:05 +0300

    Added DTO & validators

---
+++ BLL/DTO/Book/UpdateBookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL.DTO.Book
+{
+    public class UpdateBookDto
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public int IdPublisher { get; set; }
+    }
+}
---
+++ BLL/Infrastructure/Validators/Publisher/UpdatePublisherDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using BLL.DTO.Publisher;
+using FluentValidation;
+
+namespace BLL.Infrastructure.Validators.Publisher
+{
+    public class UpdatePublisherDtoValidator : AbstractValidator<UpdatePublisherDto>
+    {
+        public UpdatePublisherDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().GreaterThan(0);
+            RuleFor(x => x.Name).NotNull().NotEmpty().Length(1, 150);
+        }
+    }
+}
