c135fbd1271fdb0594911ccc82340b5f879030f3
beavonich <beavonich@gmail.com>
2022-12-23 12:09:20 +0300

    add pattern BookController

---
+++ BookStore.API/Controllers/BookController.cs
@@ -0,0 +1,121 @@
+﻿using DLL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        public BookController()
+        {
+
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return null;
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBook([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(null);
+        }
+
+        // PUT: api/Books/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook([FromRoute] int id, [FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(book).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            //try
+            //{
+            //    //await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!BookExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public async Task<IActionResult> PostBook([FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //_context.Books.Add(book);
+            //await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //var book = await _context.Books.FindAsync(id);
+            var book = new Book();
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            //_context.Books.Remove(book);
+            //await _context.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        private bool BookExists(int id)
+        {
+            //return _context.Books.Any(e => e.Id == id);
+            return false;
+        }
+
+    }
+}
