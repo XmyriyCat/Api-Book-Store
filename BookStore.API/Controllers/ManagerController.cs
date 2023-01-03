d37bdedccffa79f76536efc3551e24e56008763f
Андрей Бобровский <andrey.bobrovskiy2003@gmail.com>
2023-01-03 17:51:23 +0300

    add ManagerController

---
+++ BookStore.API/Controllers/ManagerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+    }
+}
