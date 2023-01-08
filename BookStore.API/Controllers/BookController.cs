c0ddae2ec7ca20a6983f5776b50355713a137962
Aleshkevich Pavel <aleskevic79@gmail.com>
2023-01-08 14:41:00 +0300

    Add roles in controllers

--- BookStore.API/Controllers/BookController.cs
+++ BookStore.API/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 using BLL.DTO.Book;
 using BLL.Services.Contract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiBookStore.Controllers;
@@ -15,39 +16,43 @@ public class BookController : ControllerBase
         _bookService = bookService;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<IActionResult> BookGetAllAsyncTask()
     {
         var books = await _bookService.GetAllAsync();
         return Ok(books);
     }
-    
+
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> BookGetByIdAsyncTask(int id)
     {
         var book = await _bookService.FindAsync(id);
-                
+
         if (book is null)
         {
             return NotFound();
         }
-    
+
         return Ok(book);
     }
-    
+
+    [Authorize(Roles = "Manager")]
     [HttpPost]
     public async Task<IActionResult> BookCreateAsyncTask([FromBody] CreateBookDto book)
     {
-        if (book is null) 
+        if (book is null)
         {
             return BadRequest();
         }
-    
+
         var createdBook = await _bookService.AddAsync(book);
-    
+
         return new ObjectResult(createdBook) { StatusCode = StatusCodes.Status201Created };
     }
-    
+
+    [Authorize(Roles = "Manager")]
     [HttpPut]
     public async Task<IActionResult> BookUpdateAsyncTask([FromBody] UpdateBookDto book)
     {
@@ -55,17 +60,18 @@ public class BookController : ControllerBase
         {
             return BadRequest();
         }
-    
+
         var updatedBook = await _bookService.UpdateAsync(book);
-    
+
         if (updatedBook is null)
         {
             return NotFound();
         }
-    
+
         return Ok(updatedBook);
     }
-    
+
+    [Authorize(Roles = "Manager")]
     [HttpDelete("{id}")]
     public async Task<IActionResult> BookDeleteAsyncTask(int id)
     {
