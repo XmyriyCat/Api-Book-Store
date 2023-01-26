1881d2631eb890822139a2db9f75dd83d637f519
Андрей Бобровский <andrey.bobrovskiy2003@gmail.com>
2023-01-26 19:05:48 +0300

    Create AdminController

--- BookStore.API/Controllers/AdminController.cs
+++ BookStore.API/Controllers/AdminController.cs
@@ -1,13 +1,86 @@
-using BLL.DTO.WarehouseBook;
+using BLL.DTO.User;
 using BLL.Services.Contract;
+using DLL.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiBookStore.Controllers;
 
+[ApiController]
+[Authorize(Roles = "Admin")]
 public class AdminController : ControllerBase
 {
-    public AdminController()
+    private readonly IAdminCatalogService _adminService;
+
+    public AdminController(IAdminCatalogService adminService)
     {
+        _adminService = adminService;
     }
+
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsyncTask()
+    {
+        var users = await _adminService.GetAllAsync();
+        return Ok(users);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCount()
+    {
+        var usersAmount = await _adminService.CountAsync();
+        return Ok(usersAmount);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsyncTask(int id)
+    {
+        var user = await _adminService.FindAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> DeleteByIdAsyncTask(int id)
+    {
+        await _adminService.DeleteAsync(id);
+
+        
+
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateUserAsync(UpdateUserDto updateUser)
+    {
+        var user = await _adminService.UpdateAsync(updateUser);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUserAsync(CreateUserDto createUser)
+    {
+        var user = await _adminService.AddAsync(createUser);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    
 }
