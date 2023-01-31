3f9b8eeb3d8ac779ee8b72da6a9df2e845db2798
bullinchik <happysonphil@gmail.com>
2023-01-31 21:19:13 +0300

    Add CreateBookDtoValidator and UpdateBookDtoValidator tests

---
+++ BLL.Tests/Validators/Book/CreateBookDtoValidatorTest.cs
@@ -0,0 +1,99 @@
+﻿using BLL.DTO.Author;
+using BLL.DTO.Book;
+using BLL.Infrastructure.Validators.Book;
+using Bogus;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace BLL.Tests.Validators.Book;
+
+public class CreateBookDtoValidatorTest
+{
+    private readonly IValidator<CreateBookDto> _createBookDtoValidator;
+
+    public CreateBookDtoValidatorTest()
+    {
+        _createBookDtoValidator = new CreateBookDtoValidator();
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_IdPublisher_and_Name_are_null_and_Price_is_0()
+    {
+        //Arrange
+        var faker = new Faker<CreateBookDto>()
+            .RuleFor(x => x.Name, f => null)
+            .RuleFor(x => x.IdPublisher, f => 0)
+            .RuleFor(x => x.Price, f => -1);
+
+        var createBookDto = faker.Generate();
+        
+        //Act
+        var result = await _createBookDtoValidator.TestValidateAsync(createBookDto);
+
+        //Assert
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_not_have_error()
+    {
+        //Arrange
+        var faker = new Faker<CreateBookDto>()
+            .RuleFor(x => x.Name, f => f.Random.String2(1,200))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(1))
+            .RuleFor(x=>x.Price, f => f.Random.Int(1));
+
+        var createBookDto = faker.Generate();
+        
+        //Act
+        var result = await _createBookDtoValidator.TestValidateAsync(createBookDto);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(book => book.Name);
+        result.ShouldNotHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldNotHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_Name_is_longer_200_and_Price_and_IdPublisher_are_less_0()
+    {
+        //Arrange
+        var faker = new Faker<CreateBookDto>()
+            .RuleFor(x => x.Name, f => f.Random.String2(201, 250))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(-10, -1))
+            .RuleFor(x => x.Price, f => f.Random.Int(-10, -1));
+
+        var createBookDto = faker.Generate();
+        
+        //Act
+        var result = await _createBookDtoValidator.TestValidateAsync(createBookDto);
+        
+        //Assert
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_Name_is_empty()
+    {
+        //Arrange
+        var faker = new Faker<CreateBookDto>()
+            .RuleFor(x => x.Name, f => f.Random.String2(0))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(1))
+            .RuleFor(x => x.Price, f => f.Random.Int(1));
+
+        var createBookDto = faker.Generate();
+
+        //Act
+        var result = await _createBookDtoValidator.TestValidateAsync(createBookDto);
+        
+        //Assert
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldNotHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldNotHaveValidationErrorFor(book => book.Price);
+    }
+}
---
+++ BLL.Tests/Validators/Book/UpdateBookDtoValidatorTest.cs
@@ -0,0 +1,106 @@
+﻿using BLL.DTO.Book;
+using BLL.Infrastructure.Validators.Book;
+using Bogus;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace BLL.Tests.Validators.Book;
+
+public class UpdateBookDtoValidatorTest
+{
+        private readonly IValidator<UpdateBookDto> _updateBookDtoValidator;
+
+    public UpdateBookDtoValidatorTest()
+    {
+        _updateBookDtoValidator = new UpdateBookDtoValidator();
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_IdPublisher_and_Name_are_null_and_Price_and_Is_are_0()
+    {
+        //Arrange
+        var faker = new Faker<UpdateBookDto>()
+            .RuleFor(x => x.Id, f => 0)
+            .RuleFor(x => x.Name, f => null)
+            .RuleFor(x => x.IdPublisher, f => 0)
+            .RuleFor(x => x.Price, f => -1);
+
+        var updateBookDto = faker.Generate();
+        
+        //Act
+        var result = await _updateBookDtoValidator.TestValidateAsync(updateBookDto);
+
+        //Assert
+        result.ShouldHaveValidationErrorFor(book => book.Id);
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_not_have_error()
+    {
+        //Arrange
+        var faker = new Faker<UpdateBookDto>()
+            .RuleFor(x => x.Id, f => f.Random.Int(1))
+            .RuleFor(x => x.Name, f => f.Random.String2(1,200))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(1))
+            .RuleFor(x=>x.Price, f => f.Random.Int(1));
+
+        var updateBookDto = faker.Generate();
+        
+        //Act
+        var result = await _updateBookDtoValidator.TestValidateAsync(updateBookDto);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(book => book.Id);
+        result.ShouldNotHaveValidationErrorFor(book => book.Name);
+        result.ShouldNotHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldNotHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_Name_is_longer_200_and_Price_and_IdPublisher_are_less_0()
+    {
+        //Arrange
+        var faker = new Faker<UpdateBookDto>()
+            .RuleFor(x => x.Id, f => f.Random.Int(1))
+            .RuleFor(x => x.Name, f => f.Random.String2(201, 250))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(-10, -1))
+            .RuleFor(x => x.Price, f => f.Random.Int(-10, -1));
+
+        var updateBookDto = faker.Generate();
+        
+        //Act
+        var result = await _updateBookDtoValidator.TestValidateAsync(updateBookDto);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(book => book.Id);
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldHaveValidationErrorFor(book => book.Price);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_Name_is_empty()
+    {
+        //Arrange
+        var faker = new Faker<UpdateBookDto>()
+            .RuleFor(x=> x.Id, f => f.Random.Int(1))
+            .RuleFor(x => x.Name, f => f.Random.String2(0))
+            .RuleFor(x => x.IdPublisher, f => f.Random.Int(1))
+            .RuleFor(x => x.Price, f => f.Random.Int(1));
+
+        var updateBookDto = faker.Generate();
+
+        //Act
+        var result = await _updateBookDtoValidator.TestValidateAsync(updateBookDto);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(book => book.Id);
+        result.ShouldHaveValidationErrorFor(book => book.Name);
+        result.ShouldNotHaveValidationErrorFor(book => book.IdPublisher);
+        result.ShouldNotHaveValidationErrorFor(book => book.Price);
+    }
+}
