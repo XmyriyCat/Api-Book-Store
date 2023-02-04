83c2f6e88fec626cc7100426dec836fef7e7345c
bullinchik <happysonphil@gmail.com>
2023-02-04 19:51:41 +0300

    Add user validator tests

---
+++ BLL.Tests/Validators/User/RegistrationGoogleUserDtoValidatorTest.cs
@@ -0,0 +1,74 @@
+﻿using BLL.DTO.User;
+using BLL.Infrastructure.Validators.User;
+using Bogus;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+#pragma warning disable CS8603
+
+namespace BLL.Tests.Validators.User;
+
+public class RegistrationGoogleUserDtoValidatorTest
+{
+    private readonly IValidator<RegistrationGoogleUserDto> _registrationUserDtoValidator;
+
+    public RegistrationGoogleUserDtoValidatorTest()
+    {
+        _registrationUserDtoValidator = new RegistrationGoogleUserDtoValidator();
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_values_are_null()
+    {
+        //Arrange
+        var faker = new Faker<RegistrationGoogleUserDto>()
+            .RuleFor(x => x.GoogleToken, f => null)
+            .RuleFor(x => x.Password, f => null);
+
+        var registrationGoogleUser = faker.Generate();
+        
+        //Act
+        var result = await _registrationUserDtoValidator.TestValidateAsync(registrationGoogleUser);
+        
+        //Assert
+        result.ShouldHaveValidationErrorFor(google => google.GoogleToken);
+        result.ShouldHaveValidationErrorFor(google => google.Password);
+    }
+
+    [Fact]
+    public async Task Should_not_have_error()
+    {
+        //Arrange
+        var faker = new Faker<RegistrationGoogleUserDto>()
+            .RuleFor(x => x.GoogleToken, f => f.Random.String(15))
+            .RuleFor(x => x.Password, f => f.Random.String(15));
+
+        var registrationGoogleUser = faker.Generate();
+        
+        //Act
+        var result = await _registrationUserDtoValidator.TestValidateAsync(registrationGoogleUser);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(google => google.GoogleToken);
+        result.ShouldNotHaveValidationErrorFor(google => google.Password);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_values_are_empty()
+    {
+        //Arrange
+        var faker = new Faker<RegistrationGoogleUserDto>()
+            .RuleFor(x => x.GoogleToken, f => f.Random.String(0))
+            .RuleFor(x => x.Password, f => f.Random.String(0));
+
+        var registrationGoogleUser = faker.Generate();
+        
+        //Act
+        var result = await _registrationUserDtoValidator.TestValidateAsync(registrationGoogleUser);
+        
+        //Assert
+        result.ShouldHaveValidationErrorFor(google => google.GoogleToken);
+        result.ShouldHaveValidationErrorFor(google => google.Password);
+    }
+}
---
+++ BLL.Tests/Validators/User/UpdateUserDtoValidatorTest.cs
@@ -0,0 +1,119 @@
+﻿using BLL.DTO.User;
+using BLL.Infrastructure.Validators.User;
+using Bogus;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+#pragma warning disable CS8603
+
+namespace BLL.Tests.Validators.User;
+
+public class UpdateUserDtoValidatorTest
+{
+    private readonly IValidator<UpdateUserDto> _updateUserDtoValidator;
+
+    public UpdateUserDtoValidatorTest()
+    {
+        _updateUserDtoValidator = new UpdateUserDtoValidator();
+    }
+
+    [Fact]
+    public async Task Should_error_when_values_are_null_or_less_0()
+    {
+        //Arrange
+        var faker = new Faker<UpdateUserDto>()
+            .RuleFor(x => x.Id, f => 0)
+            .RuleFor(x => x.Username, f => null)
+            .RuleFor(x => x.Login, f => null)
+            .RuleFor(x => x.Email, f=> f.Internet.Email())
+            .RuleFor(x => x.Country, f => f.Random.String2(0))
+            .RuleFor(x => x.Address, f => f.Random.String2(0))
+            .RuleFor(x => x.RolesIds, f => new List<int>()
+            {
+                f.Random.Int(-10, -1),
+                f.Random.Int(-10, -1),
+                f.Random.Int(-10, -1)
+            });
+
+        var updateUserDto = faker.Generate();
+        
+        //Act
+        var result = await _updateUserDtoValidator.TestValidateAsync(updateUserDto);
+        
+        //Assert
+        result.ShouldHaveValidationErrorFor(user => user.Id);
+        result.ShouldHaveValidationErrorFor(user => user.Username);
+        result.ShouldHaveValidationErrorFor(user => user.Login);
+        result.ShouldNotHaveValidationErrorFor(user => user.Email);
+        result.ShouldNotHaveValidationErrorFor(user => user.Country);
+        result.ShouldNotHaveValidationErrorFor(user => user.Address);
+        result.ShouldHaveValidationErrorFor(user => user.RolesIds);
+    }
+
+    [Fact]
+    public async Task Should_not_have_error()
+    {
+        //Arrange
+        var faker = new Faker<UpdateUserDto>()
+            .RuleFor(x => x.Id, f => f.Random.Int(1))
+            .RuleFor(x => x.Username, f => f.Random.String2(1, 150))
+            .RuleFor(x => x.Login, f => f.Random.String2(1, 150))
+            .RuleFor(x => x.Email, f=> f.Internet.Email())
+            .RuleFor(x => x.Country, f => f.Random.String2(1, 150))
+            .RuleFor(x => x.Address, f => f.Random.String2(1, 150))
+            .RuleFor(x => x.RolesIds, f => new List<int>()
+            {
+                f.Random.Int(1, 10),
+                f.Random.Int(1, 10),
+                f.Random.Int(1, 10)
+            });
+
+        var createUser = faker.Generate();
+        
+        //Act
+        var result = await _updateUserDtoValidator.TestValidateAsync(createUser);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(user => user.Id);
+        result.ShouldNotHaveValidationErrorFor(user => user.Username);
+        result.ShouldNotHaveValidationErrorFor(user => user.Login);
+        result.ShouldNotHaveValidationErrorFor(user => user.Email);
+        result.ShouldNotHaveValidationErrorFor(user => user.Country);
+        result.ShouldNotHaveValidationErrorFor(user => user.Address);
+        result.ShouldNotHaveValidationErrorFor(user => user.RolesIds);
+    }
+
+    [Fact]
+    public async Task Should_have_error_when_values_are_greater_150()
+    {
+        //Arrange
+        var faker = new Faker<UpdateUserDto>()
+            .RuleFor(x => x.Id, f => f.Random.Int(1))
+            .RuleFor(x => x.Username, f => f.Random.String2(151,200))
+            .RuleFor(x => x.Login, f =>  f.Random.String2(151,200))
+            .RuleFor(x => x.Email, f=> f.Internet.Email())
+            .RuleFor(x => x.Country, f =>  f.Random.String2(151,200))
+            .RuleFor(x => x.Address, f => f.Random.String2(151,200))
+            .RuleFor(x => x.RolesIds, f => new List<int>()
+            {
+                f.Random.Int(1, 10),
+                f.Random.Int(1, 10),
+                f.Random.Int(1, 10)
+            });
+
+        var updateUser = faker.Generate();
+        
+        //Act
+        var result = await _updateUserDtoValidator.TestValidateAsync(updateUser);
+        
+        //Assert
+        result.ShouldNotHaveValidationErrorFor(user => user.Id);
+        result.ShouldHaveValidationErrorFor(user => user.Username);
+        result.ShouldHaveValidationErrorFor(user => user.Login);
+        result.ShouldNotHaveValidationErrorFor(user => user.Email);
+        result.ShouldHaveValidationErrorFor(user => user.Country);
+        result.ShouldHaveValidationErrorFor(user => user.Address);
+        result.ShouldNotHaveValidationErrorFor(user => user.RolesIds);
+    }
+}
